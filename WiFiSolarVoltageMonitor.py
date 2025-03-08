2d5ecbd007e1c2e0478bed60df557eed8feef778
David Lawrence <davidl@jlab.org>
2025-03-07 22:13:14 -0500

    Change influxDB tag from 2.7.1 to 2.7.11 in comments. Change vmon_url.

--- WiFiSolarVoltageMonitor.py
+++ WiFiSolarVoltageMonitor.py
@@ -2,7 +2,7 @@
 #
 # Influx DB server may be started with:
 #
-# docker run -d -p 8086:8086 --name influxdb2 -v ${HOME}/HomeSolar/etc/influxdb2:/etc/influxdb2 -v ${HOME}/HomeSolar/DB:/var/lib/influxdb2 influxdb:2.7.1
+# docker run -d -p 8086:8086 --name influxdb2 -v ${HOME}/HomeSolar/etc/influxdb2:/etc/influxdb2 -v ${HOME}/HomeSolar/DB:/var/lib/influxdb2 influxdb:2.7.11
 #
 #
 # Start this with:
@@ -19,7 +19,8 @@ from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 from influxdb_client.domain.write_precision import WritePrecision
 
-vmon_url = "http://192.168.1.24/status.json"
+vmon_url = "http://192.168.1.17/status.json"
+if len(sys.argv)>1 : vmon_url = f"http://{sys.argv[1]}/status.json"
 #vmon_url = "http://192.168.1.81/status.json"
 influxdb_host = "http://localhost:8086"
 WRITE_TO_DB = True
@@ -29,6 +30,8 @@ port=8086
 db_token = 'OQoSwarVK9qtnN9GvZ818IL7Su1VlZoJdqrSeRH8kreFyWdIqsAH8b0zpOpdinnbjuh4GpvM3gH7x956QnDo3A=='
 db_org = 'Hill-Lawrence'
 db_bucket = 'HL_Solar'
+
+print(f"Data will be read from device at: {vmon_url}")
 
 print("Connecting to DB ...")
 # client = InfluxDBClient(host, port, dbuser, dbuser_password, dbname)
